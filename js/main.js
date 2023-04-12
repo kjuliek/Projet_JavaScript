e297b6c3a8374a2803465e3f93a347a6f9164971
kjuliek <112950414+kjuliek@users.noreply.github.com>
2023-04-12 16:53:44 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// =============================================================================
+// game states
+// =============================================================================
+
+PlayState = {};
+
+PlayState.preload = function () {
+    this.game.load.json('level:1', 'data/level02.json');
+
+    this.game.load.image('background', 'images/background.png');
+    this.game.load.image('ground', 'images/ground.png');
+    this.game.load.image('underground', 'images/underground.png')
+    this.game.load.image('grass:8x1', 'images/snow_8x1.png');
+    this.game.load.image('grass:6x1', 'images/snow_6x1.png');
+    this.game.load.image('grass:4x1', 'images/snow_4x1.png');
+    this.game.load.image('grass:2x1', 'images/snow_2x1.png');
+    this.game.load.image('grass:1x1', 'images/snow_1x1.png');
+};
+
+PlayState.create = function () {
+    this.game.add.image(0, 0, 'background');
+    this._loadLevel(this.game.cache.getJSON('level:1'));
+};
+
+PlayState._loadLevel = function (data) {
+    // spawn all platforms
+    data.platforms.forEach(this._spawnPlatform, this);
+};
+
+PlayState._spawnPlatform = function (platform) {
+    this.game.add.sprite(platform.x, platform.y, platform.image);
+};
+
+// =============================================================================
+// entry point
+// =============================================================================
+
+window.onload = function () {
+    let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
+
+    game.state.add('play', PlayState);
+    game.state.start('play');
+};
