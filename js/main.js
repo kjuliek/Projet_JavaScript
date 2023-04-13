75fd7c6be0856aec5b71274c8407f801e42d63a2
kjuliek <112950414+kjuliek@users.noreply.github.com>
2023-04-13 15:06:08 +0100

    gestion des déplacements du hero

--- js/main.js
+++ js/main.js
@@ -1,20 +1,70 @@
 // =============================================================================
+// sprites
+// =============================================================================
+
+//
+// hero sprite
+//
+function Hero(game, x, y) {
+    Phaser.Sprite.call(this, game, x, y, 'hero');
+    this.game.physics.enable(this);
+    this.body.collideWorldBounds = true;
+
+    this.anchor.set(0.5, 0.5);
+}
+
+// inherit from Phaser.Sprite
+Hero.prototype = Object.create(Phaser.Sprite.prototype);
+Hero.prototype.constructor = Hero;
+
+Hero.prototype.move = function (direction) {
+    const SPEED = 200;
+    this.body.velocity.x = direction * SPEED;
+};
+
+Hero.prototype.jump = function () {
+    const JUMP_SPEED = 600;
+    let canJump = this.body.touching.down;
+
+    if (canJump) {
+        this.body.velocity.y = -JUMP_SPEED;
+    }
+
+    return canJump;
+};
+
+// =============================================================================
 // game states
 // =============================================================================
 
 PlayState = {};
+
+PlayState.init = function () {
+    this.game.renderer.renderSession.roundPixels = true;
+
+    this.keys = this.game.input.keyboard.addKeys({
+        left: Phaser.KeyCode.LEFT,
+        right: Phaser.KeyCode.RIGHT,
+        up: Phaser.KeyCode.UP
+    });
+    this.keys.up.onDown.add(function () {
+        this.hero.jump();
+    }, this);
+};
 
 PlayState.preload = function () {
     this.game.load.json('level:1', 'data/level02.json');
 
     this.game.load.image('background', 'images/background.png');
     this.game.load.image('ground', 'images/ground.png');
-    this.game.load.image('underground', 'images/underground.png')
+    this.game.load.image('underground', 'images/underground.png');
     this.game.load.image('grass:8x1', 'images/snow_8x1.png');
     this.game.load.image('grass:6x1', 'images/snow_6x1.png');
     this.game.load.image('grass:4x1', 'images/snow_4x1.png');
     this.game.load.image('grass:2x1', 'images/snow_2x1.png');
     this.game.load.image('grass:1x1', 'images/snow_1x1.png');
+    this.game.load.image('hero', 'images/hero_stopped.png');
+    //this.game.load.spritesheet('hero', 'images/hero_animated.png', 72, 94);
 };
 
 PlayState.create = function () {
@@ -22,13 +72,56 @@ PlayState.create = function () {
     this._loadLevel(this.game.cache.getJSON('level:1'));
 };
 
+PlayState.update = function () {
+    this._handleCollisions();
+    this._handleInput();
+};
+
+PlayState._handleCollisions = function () {
+    this.game.physics.arcade.collide(this.hero, this.platforms);
+};
+
+PlayState._handleInput = function () {
+    if (this.keys.left.isDown) { // move hero left
+        this.hero.move(-1);
+    }
+    else if (this.keys.right.isDown) { // move hero right
+        this.hero.move(1);
+    }
+    else {
+        this.hero.move(0);
+    }
+};
+
 PlayState._loadLevel = function (data) {
+    // create all the groups/layers that we need
+    this.platforms = this.game.add.group();
+
     // spawn all platforms
     data.platforms.forEach(this._spawnPlatform, this);
+    // spawn hero and enemies
+    this._spawnCharacters({hero: data.hero, spiders: data.spiders});
+
+    // enable gravity
+    const GRAVITY = 1200;
+    this.game.physics.arcade.gravity.y = GRAVITY;
 };
 
 PlayState._spawnPlatform = function (platform) {
-    this.game.add.sprite(platform.x, platform.y, platform.image);
+    let sprite = this.platforms.create(
+        platform.x, platform.y, platform.image);
+
+    this.game.physics.enable(sprite);
+    sprite.body.allowGravity = false;
+    sprite.body.immovable = true;
+
+};
+
+PlayState._spawnCharacters = function (data) {
+    // spawn hero
+    this.hero = new Hero(this.game, data.hero.x, data.hero.y);
+    this.game.add.existing(this.hero);
+
 };
 
 // =============================================================================
@@ -37,7 +130,6 @@ PlayState._spawnPlatform = function (platform) {
 
 window.onload = function () {
     let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
-
     game.state.add('play', PlayState);
     game.state.start('play');
 };
